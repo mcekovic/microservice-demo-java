4b50afee360a670dabb2095b4c7cd0956945dad3
Mileta Cekovic <mcekovic@gmail.com>
2021-12-22 13:33:25 +0100

    Update

--- src/main/java/com/igt/demo/microservice/HelloConfiguration.java
+++ src/main/java/com/igt/demo/microservice/HelloConfiguration.java
@@ -1,19 +1,13 @@
 package com.igt.demo.microservice;
 
+import lombok.*;
 import org.springframework.boot.context.properties.*;
 import org.springframework.context.annotation.*;
 
 @Configuration
 @ConfigurationProperties("hello")
+@Getter @Setter
 public class HelloConfiguration {
 
 	private String defaultName = "Stranger";
-
-	public String getDefaultName() {
-		return defaultName;
-	}
-
-	public void setDefaultName(String defaultName) {
-		this.defaultName = defaultName;
-	}
 }
